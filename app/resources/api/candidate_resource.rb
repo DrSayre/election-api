891679dadd3904a8f16334c2c595ef7394d1188f
Caleb Sayre <DrSayre2002@yahoo.com>
2016-05-28 18:53:36 -0400

    tweaks

--- app/resources/api/candidate_resource.rb
+++ app/resources/api/candidate_resource.rb
@@ -2,6 +2,10 @@ module Api
   # app/resources/api/candidate_resource.rb
   class CandidateResource < JSONAPI::Resource
     has_many :mean_samples
-    attributes :name, :probability, :magic_8_ball
+    attributes :name, :probability, :status
+
+    def status
+      @model.magic_8_ball
+    end
   end
 end
