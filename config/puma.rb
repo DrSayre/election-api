8f30a10a2401aaee1140d44555f4967b5544912b
Caleb Sayre <DrSayre2002@yahoo.com>
2016-06-05 12:46:19 -0400

    trying something different

--- config/puma.rb
+++ config/puma.rb
@@ -48,30 +48,60 @@
 
 #!/usr/bin/env puma
 
-directory '/home/deploy/apps/election-api/current'
-rackup "/home/deploy/apps/election-api/current/config.ru"
-environment 'production'
+# directory '/home/deploy/apps/election-api/current'
+# rackup "/home/deploy/apps/election-api/current/config.ru"
+# environment 'production'
 
-pidfile "/home/deploy/apps/election-api/shared/tmp/pids/puma.pid"
-state_path "/home/deploy/apps/election-api/shared/tmp/pids/puma.state"
-stdout_redirect '/home/deploy/apps/election-api/current/log/puma.error.log', '/home/deploy/apps/election-api/current/log/puma.access.log', true
-threads 1,6
-bind 'unix:///home/deploy/apps/election-api/shared/tmp/sockets/election-api-puma.sock'
+# pidfile "/home/deploy/apps/election-api/shared/tmp/pids/puma.pid"
+# state_path "/home/deploy/apps/election-api/shared/tmp/pids/puma.state"
+# stdout_redirect '/home/deploy/apps/election-api/current/log/puma.error.log', '/home/deploy/apps/election-api/current/log/puma.access.log', true
+# threads 1,6
+# bind 'unix:///home/deploy/apps/election-api/shared/tmp/sockets/election-api-puma.sock'
 
-workers 0
-preload_app!
-on_restart do
-  puts 'Refreshing Gemfile'
-  ENV["BUNDLE_GEMFILE"] = "/home/deploy/apps/election-api/current/Gemfile"
-end
+# workers 1
+# preload_app!
+# on_restart do
+#   puts 'Refreshing Gemfile'
+#   ENV["BUNDLE_GEMFILE"] = "/home/deploy/apps/election-api/current/Gemfile"
+# end
 
 
-before_fork do
-  ActiveRecord::Base.connection_pool.disconnect!
-end
+# before_fork do
+#   ActiveRecord::Base.connection_pool.disconnect!
+# end
+
+# on_worker_boot do
+#   ActiveSupport.on_load(:active_record) do
+#     ActiveRecord::Base.establish_connection
+#   end
+# end
+
+# Change to match your CPU core count
+workers 1
+
+# Min and Max threads per worker
+threads 1, 6
+
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/shared"
+
+# Default to production
+rails_env = ENV['RAILS_ENV'] || "production"
+environment rails_env
+
+# Set up socket location
+bind "unix://#{shared_dir}/sockets/election-api-puma.sock"
+
+# Logging
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+
+# Set master PID and state locations
+pidfile "#{shared_dir}/pids/election-api-puma.pid"
+state_path "#{shared_dir}/pids/election-api-puma.state"
+activate_control_app
 
 on_worker_boot do
-  ActiveSupport.on_load(:active_record) do
-    ActiveRecord::Base.establish_connection
-  end
+  require "active_record"
+  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
 end
