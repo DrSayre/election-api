435c650d4d02b8ed6c876ffb07931413982276ba
Caleb Sayre <DrSayre2002@yahoo.com>
2016-06-05 23:01:26 -0400

    using clockwork

--- config/initializers/enque_update_numbers_job.rb
+++ config/initializers/enque_update_numbers_job.rb
@@ -1,4 +1,4 @@
 # print "\n***********\nHello World\n***********\n"
-UpdateNumbersJob.perform_later
-UpdateMeanSampleJob.set(wait: 1.minute).perform_later
-TweetStatusJob.set(wait: 3.minutes).perform_later
+# UpdateNumbersJob.perform_later
+# UpdateMeanSampleJob.set(wait: 1.minute).perform_later
+# TweetStatusJob.set(wait: 3.minutes).perform_later
