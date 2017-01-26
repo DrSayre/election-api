cf87f384dbb5508d0773e89c573b9617f5d09848
Caleb Sayre <DrSayre2002@yahoo.com>
2017-01-25 22:22:04 -0500

    removed stopping clock

--- clock.rb
+++ clock.rb
@@ -7,8 +7,8 @@ module Clockwork
     puts "Running #{job}\n#{File.join(File.dirname(__FILE__))}"
   end
 
-  every(15.minutes, 'Update numbers') { UpdateNumbersJob.perform_now }
-  every(4.hours, 'Update Twitter') { TweetStatusJob.perform_now }
-  every(1.day, 'Update Mean Sample') { UpdateMeanSampleJob.perform_now }
+  # every(15.minutes, 'Update numbers') { UpdateNumbersJob.perform_now }
+  # every(4.hours, 'Update Twitter') { TweetStatusJob.perform_now }
+  # every(1.day, 'Update Mean Sample') { UpdateMeanSampleJob.perform_now }
   # every(1.day, 'Positions expiring today emails', at: '09:00') { MultipleEmail.positions_expiring_today }
 end
--- config/puma.rb
+++ config/puma.rb
@@ -86,7 +86,7 @@ app_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
 
 # Default to production
-rails_env = ENV['RAILS_ENV'] || "production"
+rails_env = ENV['RAILS_ENV'] || "development"
 environment rails_env
 
 # Set up socket location
@@ -101,7 +101,6 @@ state_path "#{shared_dir}/pids/election-api-puma.state"
 activate_control_app
 
 on_worker_boot do
-  print "\n*******\nHello World\n********\n"
   require "active_record"
   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
   ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
